b03d02814c9811243b6ef0e2c9609e3f16fffc43
Guido Da Re <98586726+gda98re@users.noreply.github.com>
2023-03-10 00:23:18 +0100

    Delete ST_bundle directory

--- ST_bundle/__init__.py
+++
@@ -1,9 +0,0 @@
-from ST_bundle.ST_bundle import ST_bundle
-from ST_bundle.tindices_object import tindices_object
-from ST_bundle._scalar import _scalar
-from ST_bundle._spin_tensor import _spin_tensor
-from ST_bundle._sconnection import _sconnection
-from ST_bundle._tconnection import _tconnection
-from ST_bundle._totconnection import _totconnection
-
-__version__ = '0.1.0'
